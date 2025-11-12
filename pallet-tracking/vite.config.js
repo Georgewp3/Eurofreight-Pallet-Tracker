16fa29d335af30e5ddc24766c000068dd634f92d
Georgewp3 <2335150@brunel.ac.uk>
2025-11-12 16:45:12 +0000

    Removed all files, made the project simpler

--- pallet-tracking/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// IMPORTANT: set base to your repo name for GitHub Pages
-export default defineConfig({
-  plugins: [react()],
-  base: "/pallet-tracking/",
-});
